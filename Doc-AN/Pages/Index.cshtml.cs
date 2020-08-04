9d5c5f82fc1526e4fd1e0448a1c48ed8a6d2a986
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-08-04 21:24:08 +0300

    Create simple ASP Net Core app with Dockerfile and docker-compose.yml

---
+++ Doc-AN/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Doc_AN.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel() { }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
